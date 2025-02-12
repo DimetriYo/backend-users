43feae5ba7835b1da0151205f63df18e36a0f1a1
m5production <dimikh@mail.ru>
2025-02-12 13:00:04 +0300

    feat: leace only backend in repo

---
+++ src/db/users.js
@@ -0,0 +1,6 @@
+export const users = [
+  { id: "0", name: "Dmitriy", lastname: "Mikheyev", role: "admin" },
+  { id: "1", name: "Alla", lastname: "Velichko", role: "user" },
+  { id: "2", name: "Torin", lastname: "Mikheyev", role: "user" },
+  { id: "3", name: "Georgiy", lastname: "Velichko", role: "user" },
+]
